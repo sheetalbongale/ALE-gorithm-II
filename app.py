ce1397028e13f1f510e180b575c49372f9df44b5
Barry <semieng94@gmail.com>
2020-05-04 20:09:44 -0500

    updated app.py for gcp

--- app.py
+++ app.py
@@ -256,4 +256,4 @@ def predict():
 #                           Main                               #
 ################################################################
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
