18b8c02aeb56ec4de71bf6917ccadac6ef8c3dd0
Catherine Gomes <catherinegomes@users.noreply.github.com>
2020-05-06 19:46:57 -0500

    update with master

--- app.py
+++ app.py
@@ -8,29 +8,29 @@ import json
 import pandas as pd
 from flask import Response
 import os
-from surprise import Dataset
-from surprise import Reader
-from surprise import KNNBasic
-from surprise.model_selection import train_test_split
 from surprise import dump
 
-# Path to dump file and name
-dumpfile = os.path.join('./data/dump/dump_knn_pearsonbaseline_500dump_file')
-beer_pickel_path = os.path.join('./data/dump/beer.pkl')
+
+# Path to dump files and name
+dumpfile_knn = os.path.join('./data/dump/dump_knn_dump_file')
+dumpfile_svd = os.path.join('./data/dump/dump_svd_dump_file')
+beer_pickel_path = os.path.join('./data/dump/beer_final.pkl')
 
 # Load dump files
-predictions,algo = dump.load(dumpfile)
+predictions_knn,algo_knn = dump.load(dumpfile_knn)
+predictions_svd,algo_svd = dump.load(dumpfile_svd)
 beers_df = pd.read_pickle(beer_pickel_path)
 
-# Create the trainset from the algo
-trainset = algo.trainset
+# Create the trainset from the knn_algorithm in order to get the inner_ids
+trainset_knn = algo_knn.trainset
 
-def get_beer_name (beer_raw_id):
-    beer_name = beers_df.loc[beers_df.beer_id==beer_raw_id,'name'].values[0]
-    return beer_name
+
+def get_beer_brewery (beer_raw_id):
+    beer_brewery = beers_df.loc[beers_df.beer_id==beer_raw_id,'beer_brewery'].values[0]
+    return beer_brewery
 
 def get_beer_raw_id (beer_name):
-    beer_raw_id = beers_df.loc[beers_df.name==beer_name,'beer_id'].values[0]
+    beer_raw_id = beers_df.loc[beers_df.beer_brewery==beer_name,'beer_id'].values[0]
     return beer_raw_id
 
 def get_beer_style (beer_raw_id):
@@ -42,28 +42,31 @@ def get_beer_score_mean (beer_raw_id):
     return score_mean
 
 def get_beer_neighbors (beer_raw_id):
-    beer_inner_id = algo.trainset.to_inner_iid(beer_raw_id)
-    beer_neighbors = algo.get_neighbors(beer_inner_id, k=10)
-    beer_neighbors = (algo.trainset.to_raw_iid(inner_id)
+    beer_inner_id = algo_knn.trainset.to_inner_iid(beer_raw_id)
+    beer_neighbors = algo_knn.get_neighbors(beer_inner_id, k=10)
+    beer_neighbors = (algo_knn.trainset.to_raw_iid(inner_id)
                   for inner_id in beer_neighbors)
     return(beer_neighbors)
 
 def get_beer_recc_df (beer_raw_id):
-    beer_inner_id = algo.trainset.to_inner_iid(beer_raw_id)
-    beer_neighbors = algo.get_neighbors(beer_inner_id, k=10)
-    beer_neighbors = (algo.trainset.to_raw_iid(inner_id)
+    beer_inner_id = algo_knn.trainset.to_inner_iid(beer_raw_id)
+    beer_neighbors = algo_knn.get_neighbors(beer_inner_id, k=10)
+    beer_neighbors = (algo_knn.trainset.to_raw_iid(inner_id)
                       for inner_id in beer_neighbors)
     beers_id_recc = []
-    beer_name_recc =[]
+    beer_brewery_recc =[]
     beer_style_recc = []
     beer_score_mean = []
     for beer in beer_neighbors:
         beers_id_recc.append(beer)
-        beer_name_recc.append(get_beer_name(beer))
+        beer_brewery_recc.append(get_beer_brewery(beer))
         beer_style_recc.append(get_beer_style(beer))
         beer_score_mean.append(get_beer_score_mean(beer))
-    beer_reccomendations_df = pd.DataFrame(list(zip(beers_id_recc,beer_name_recc,beer_style_recc,beer_score_mean)),
-                                       columns=['beer_id', 'name', 'style', 'score_mean'])
+    beer_reccomendations_df = pd.DataFrame(list(zip(beers_id_recc,
+                                                    beer_brewery_recc,
+                                                    beer_style_recc,
+                                                    beer_score_mean)),
+                                       columns=['beer_id', 'name','style', 'score_mean'])
     return beer_reccomendations_df
 
 ################################################################
@@ -237,12 +240,10 @@ def breweries():
 
 @app.route("/search.html")
 def recommender_selector():
-    beers = beers_df['name'].tolist()
+    beers = beers_df['beer_brewery'].tolist()
     beers.sort()
     beers.insert(0, "Choose a Beer")
     return render_template("search.html", beers = beers)
-
-
 
 @app.route("/neighbors/<beer_name>")
 def nearest_neighbors(beer_name):
@@ -259,9 +260,26 @@ def predict():
     username = data_dict["username"]
     beer_name = data_dict["beer"]
     beer_raw_id = get_beer_raw_id(beer_name)
-    predict = algo.predict(username, beer_raw_id)
+    predict = algo_knn.predict(username, beer_raw_id)
     df_predict = pd.DataFrame([predict], columns=['username', 'beer_id', 'r_ui', 'estimate', 'details'])
     return Response(df_predict.to_json(orient = "records"), mimetype='application/json')
+
+@app.route("/userpredict/<username>")
+def userpredict(username):
+    beers = beers_df['beer_brewery'].tolist()
+    predict_df = pd.DataFrame([])
+    for beer in beers:
+        beer_raw_id = get_beer_raw_id(beer)
+        predict = algo_knn.predict(username, beer_raw_id)
+        predict_df = predict_df.append(pd.DataFrame([predict], columns=['username', 'beer_id', 'r_ui', 'estimate', 'details']))
+    picks = pd.merge(predict_df, beers_df, on='beer_id')
+    top_10picks = picks.sort_values(by=['estimate'],ascending= False)[:10]
+    top_10picks['pick'] = 'Top10'
+    bot_10picks = picks.sort_values(by=['estimate'],ascending= False)[-10:]
+    bot_10picks['pick'] = 'Bottom10'
+    user_picks = pd.concat([top_10picks, bot_10picks])
+    
+    return Response(user_picks.to_json(orient = "records"), mimetype='application/json')
     
 
 ################################################################
