f8e10e8a3adbfc7bb1154da0e47a387d8dfc5b3b
sheetalbongale <57145360+sheetalbongale@users.noreply.github.com>
2020-05-07 22:20:55 -0500

    Final knn-recommender

--- static/js/knn_recommender.js
+++ static/js/knn_recommender.js
@@ -2,54 +2,33 @@
 
 // Function to display top 10 beer recommenders using KNN model
 function buildKNNRecommender(beer_name){
-
-    var count = 0;
+    
     
     d3.json(`/neighbors/${beer_name}`).then(data => data.forEach(e => {
-
         console.log(data)
 
-        if(count<5){
-            let cardBody = d3.select("#top5neighbors")
-                .append("div")
-                .classed("card", true)
-                .append("div")
-                .classed("card-body", true)
-            cardBody.append("h3")
-                .classed("card-title", true)
-                .text(e.name)
-            cardBody.append("h6")
-                .classed("card-subtitle mb-2 text-muted", true)
-                .text(`Beer Style: ${e.style}`)
-            cardBody.append("p")
-                .classed("card-text", true)
-                .text(`BeerAdvocate Score: ${e.score_mean}`)
-            }
-
-        else if (count>=5){
-            cardBody = d3.select("#next5neighbors")
-                .append("div")
-                .classed("card", true)
-                .append("div")
-                .classed("card-body", true)
-            cardBody.append("h3")
-                .classed("card-title", true)
-                .text(e.name)
-            cardBody.append("h6")
-                .classed("card-subtitle mb-2 text-muted", true)
-                .text(`Beer Style: ${e.style}`)
-            cardBody.append("p")
-                .classed("card-text", true)
-                .text(`BeerAdvocate Score: ${e.score_mean}`)
-        }
-        count++;
+        
+        let cardBody = d3.select("#top5neighbors")
+            .append("div")
+            .classed("card", true)
+            .append("div")
+            .classed("card-body", true)
+        cardBody.append("h3")
+            .classed("card-title", true)
+            .text(e.name)
+        cardBody.append("h6")
+            .classed("card-subtitle mb-2 text-muted", true)
+            .text(`Beer Style: ${e.style}`)
+        cardBody.append("p")
+            .classed("card-text", true)
+            .text(`BeerAdvocate Score: ${e.score_mean}`)
     }))
 };
 
 
 //-----------------FUNCTION INITIATOR-----------------//
 function init() {
-    d3.select('#top10neighbors').html(""),    
+    d3.select('#top5neighbors').html(""),    
     buildKNNRecommender(beer_name);
 
 }
@@ -59,7 +38,7 @@ function optionChangedFour(newBeer) {
     beer_name = newBeer;
     console.log(beer_name)
 
-    d3.select('#top10neighbors').html(""),
+    d3.select('#top5neighbors').html(""),
     buildKNNRecommender(beer_name);
 
 }
--- static/js/userpredict.js
+++ static/js/userpredict.js
@@ -2,10 +2,13 @@
 const dropdown = d3.selectAll(".dropdown-menu a")
 
 // Add a H2 element
-const header = d3.select('#top10').append("h2")
-
+const topHeader = d3.select('#top10').append("h2")
 // Placing the table here (outside of a function), ensures that regardless of the number of times the button is selected, only one table appears
-const table = d3.select('#top10').append("table")
+const topTable = d3.select('#top10').append("table")
+
+// Repeat 5 and 7 to create Header and Table for Bottom 10
+const botHeader = d3.select('#top10').append("h2")
+const botTable = d3.select('#top10').append("table")
 
 // Function to predict user preferences
 function predictRatings(){
@@ -18,26 +21,46 @@ function predictRatings(){
   d3.json(`/userpredict/${username}`).then(data => {
     console.log(data)
 
-    header.text("We're confident you'll enjoy the following beers.")
+    topHeader.text("We're confident you'll enjoy the following beers.")
+    botHeader.text("Steer clear of these beers, you'll thank us later.")
 
     // Wipes the prior table and fills the table with the desired filters
-    table.html("")
-    const headerRow = table.append("thead").append("tr")
+    topTable.html("")
+    botTable.html("")
+    let headerRow = topTable.append("thead").append("tr")
     headerRow.append("th").text("Prediction")
     headerRow.append("th").text("Score")
     headerRow.append("th").text("Name")
     headerRow.append("th").text("Style")
     headerRow.append("th").text("Brewery")
 
-    const tbody = table.append("tbody")
+    const topTbody = topTable.append("tbody")
+
+    headerRow = botTable.append("thead").append("tr")
+    headerRow.append("th").text("Prediction")
+    headerRow.append("th").text("Score")
+    headerRow.append("th").text("Name")
+    headerRow.append("th").text("Style")
+    headerRow.append("th").text("Brewery")
+
+    const botTbody = botTable.append("tbody")
 
     data.forEach(dataRow => {
-      let row = tbody.append("tr")
-      row.append("td").text(dataRow.prediction)
-      row.append("td").text(dataRow.score)
-      row.append("td").text(dataRow.name)
-      row.append("td").text(dataRow.style)
-      row.append("td").text(dataRow.brewery_name)
+      if (dataRow.pick === 'Top10') {
+        let row = topTbody.append("tr")
+        row.append("td").text(dataRow.prediction)
+        row.append("td").text(dataRow.score)
+        row.append("td").text(dataRow.name)
+        row.append("td").text(dataRow.style)
+        row.append("td").text(dataRow.brewery_name)
+      } else {
+        let row = botTbody.append("tr")
+        row.append("td").text(dataRow.prediction)
+        row.append("td").text(dataRow.score)
+        row.append("td").text(dataRow.name)
+        row.append("td").text(dataRow.style)
+        row.append("td").text(dataRow.brewery_name)
+      }
     })
   })
 };
